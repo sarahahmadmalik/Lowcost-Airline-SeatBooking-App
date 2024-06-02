609e67c4fee61ef568514b8a24434fa60fd594c1
Sara Ahmad Malik <sarahahmadmalik55@gmail.com>
2024-06-02 18:16:08 +0500

    chore(server): Removed route folder

--- server/routes/SeatRoutes.js
+++
@@ -1,8 +0,0 @@
-// src/routes/seatRoutes.js
-import { Router } from 'express';
-const router = Router();
-import { getSeatMap } from '../../controllers/seatController';
-
-router.get('/', getSeatMap);
-
-export default router;
